03d7f048b938dc9e88144878bc6ecdb39bbc7fd8
Emmanuella <emmavduru@gmail.com>
2020-11-17 13:42:21 +0100

    Add CRUD functionality for posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -7,8 +7,7 @@ module.exports.posts_get = async(req, res) => {
 
     try {
         const user = await User.findOne({ username });
-        const posts = await Post.find({ author: user._id });
-        console.log(posts);
+        const posts = await Post.find({ author: user._id }).populate("author");
         res.render("posts", { username: username, loggedUser: loggedUser, posts: posts });
     } catch (err) {
         res.json({ err });
@@ -30,6 +29,64 @@ module.exports.post_create_post = async (req, res) => {
         const post = await Post.create({ title: title, body: body, author: user._id });
         res.json({ post });
     } catch (err){
+        console.log(err);
+    }
+}
+
+module.exports.all_posts = async(req, res) => {
+    const loggedUser = res.locals.user;
+
+    try {
+        const posts = await Post.find({}).populate("author");
+        res.render("allPosts", { loggedUser: loggedUser, posts: posts });
+    } catch (err) {
+        console.log(err);
+    }
+}
+module.exports.post_detail = async(req, res) => {
+    const loggedUser = res.locals.user;
+    const { slug } = req.params;
+
+    try {
+        const post = await Post.findOne({ slug }).populate("author");
+        res.render("postDetail", { loggedUser: loggedUser, post: post });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports.post_edit_get = async(req, res) => {
+    const loggedUser = res.locals.user;
+    const { slug } = req.params;
+
+    try {
+        const post = await (await Post.findOne({ slug })).populate("author");
+        res.render("postEdit", { loggedUser: loggedUser, post: post });
+    } catch(err) {
+        console.log(err);
+    }
+
+}
+
+module.exports.post_edit_post = async(req, res) => {
+    const { title, body } = req.body;
+
+    try {
+        const post = await Post.findOneAndUpdate({ title }, { title, body});
+        res.json({ post });
+    } catch(err){
+        console.log(err);
+    }
+}
+
+module.exports.post_delete = async(req, res) => {
+    const { slug } = req.params;
+    const { username } = res.locals.user;
+
+    try {
+        await Post.findOneAndDelete({ slug });
+        res.redirect(`/${username}/posts`);
+    } catch(err) {
         console.log(err);
     }
 }
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -8,4 +8,14 @@ router.get("/:username/posts/create", postController.post_create_get);
 
 router.post("/:username/posts/create", postController.post_create_post);
 
+router.get("/posts", postController.all_posts);
+
+router.get("/:username/posts/:slug", postController.post_detail);
+
+router.get("/:username/posts/:slug/edit", postController.post_edit_get);
+
+router.post("/:username/posts/:slug/edit", postController.post_edit_post);
+
+router.post("/:username/posts/:slug/delete", postController.post_delete);
+
 module.exports = router;
